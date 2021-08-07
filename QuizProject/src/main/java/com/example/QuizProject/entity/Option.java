552ee365fde009a69dd90c050e347549eb31ffda
xchen92 <xchen92@u.rochester.edu>
2021-08-07 18:18:27 -0400

    update option

--- QuizProject/src/main/java/com/example/QuizProject/entity/Option.java
+++ QuizProject/src/main/java/com/example/QuizProject/entity/Option.java
@@ -5,6 +5,7 @@ import javax.persistence.*;
 @Entity
 @Table(name="option")
 public class Option {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "option_id")
@@ -15,5 +16,32 @@ public class Option {
 
     @Column(name = "question_id")
     private Integer question_id;
+
+    Option(){}
+
+    public Integer getOption_id() {
+        return option_id;
+    }
+
+    public void setOption_id(Integer option_id) {
+        this.option_id = option_id;
+    }
+
+    public String getOption_text() {
+        return option_text;
+    }
+
+    public void setOption_text(String option_text) {
+        this.option_text = option_text;
+    }
+
+    public Integer getQuestion_id() {
+        return question_id;
+    }
+
+    public void setQuestion_id(Integer question_id) {
+        this.question_id = question_id;
+    }
+
 
 }
